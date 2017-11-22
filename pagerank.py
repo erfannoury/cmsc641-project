b7d6ff18f60e0b4090ce2de39ca7b8e6048d02cd
Erfan Noury <erfan.noury@outlook.com>
2017-11-22 02:39:23 -0500

    Fix bug with PageRank and add MCPageRank

--- pagerank.py
+++ pagerank.py
@@ -10,14 +10,15 @@ class PageRank():
 
     Paramteres
     ----------
-    damping_factor: float
-        The damping factor, the probability that the random surfer will jump
-        to an arbitrary web page on the web.
+    teleporting_prob: float
+        The teleporting probability, the probability that the random surfer
+        will jump to an arbitrary web page on the web.
     """
-    def __init__(self, damping_factor):
-        if not (0 <= damping_factor <= 1):
-            raise ValueError('Damping factor should be in the range [0, 1].')
-        self.damping_factor = damping_factor
+    def __init__(self, teleporting_prob):
+        if not (0 <= teleporting_prob <= 1):
+            raise ValueError('Teleporting probabilityt should be in the '
+                             'range [0, 1].')
+        self.teleporting_prob = teleporting_prob
 
     def calculate(self, adj_mat):
         """
@@ -48,12 +49,14 @@ class PageRank():
             adj_mat,
             adj_mat.sum(axis=0, keepdims=True) + np.zeros_like(adj_mat),
             where=(adj_mat > 0))
-        M = (1 - self.damping_factor) * A + \
-            self.damping_factor * np.ones_like(A)
 
-        _, vr = sc.linalg.eig(M)
-        page_rank = vr[:, 0] / np.sum(vr[:, 0])
-        return page_rank
+        M = (1 - self.teleporting_prob) * A + \
+            self.teleporting_prob * np.ones_like(A)
+
+        w, vr = np.linalg.eig(M)
+        max_idx = np.argmax(np.real(w))
+        page_rank = vr[:, max_idx] / np.sum(vr[:, max_idx])
+        return np.real(page_rank)
 
     def iterative_calculate(self, adj_mat, iter_count):
         """
@@ -84,12 +87,70 @@ class PageRank():
             adj_mat,
             adj_mat.sum(axis=0, keepdims=True) + np.zeros_like(adj_mat),
             where=(adj_mat > 0))
-        M = (1 - self.damping_factor) * A + \
-            self.damping_factor * np.ones_like(A)
+        M = (1 - self.teleporting_prob) * A + \
+            self.teleporting_prob * np.ones_like(A)
 
         z = np.ones((M.shape[0], 1))
         for _ in range(iter_count):
             z = np.matmul(M, z)
 
         page_rank = z / z.sum()
+        return page_rank.reshape((-1, ))
+
+
+class MCPageRank():
+    """
+    This is an implementation of the Monte Carlo PageRank algorithm for web
+    page importance analysis in the web.
+
+    Paramteres
+    ----------
+    teleporting_prob: float
+        The teleporting probability, the probability that the random surfer
+        will jump to an arbitrary web page on the web.
+    """
+
+    def __init__(self, teleporting_prob):
+        if not (0 <= teleporting_prob <= 1):
+            raise ValueError('Teleporting probability should be in the '
+                             'range [0, 1].')
+        self.teleporting_prob = teleporting_prob
+
+    def calculate(self, adj_list, num_iter):
+        """
+        Calculates the PageRank value for each webpage using the PageRank
+        algorithm. In the Monte Carlo variant of the PageRank algorithm, each
+        node is visited at each iteration and a random walk is initiated from
+        the given node to find a final node when the random walk stops. By
+        counting the number of stops for each node, we can get an estimate
+        of the PageRank value for each node.
+
+        Parameters
+        ----------
+        adj_list: list
+            Adjacency list of the web matrix. Each element of the list is a
+            list of indices that have connections to the corresponding node.
+        num_iter: int
+            Number of iterations to run the Monte Carlo PageRank algorithm.
+
+        Returns
+        -------
+        page_rank: np.ndarray
+            A vector indicating the PageRank value for each webpage.
+        """
+        assert type(adj_list) == list, 'The adjacency list should be a list'
+
+        n = len(adj_list)
+        page_rank = np.zeros((n, ), dtype=np.float64)
+        for _ in range(num_iter):
+            for j in range(n):
+                idx = j
+                while np.random.random() > self.teleporting_prob:
+                    if len(adj_list[idx]) == 0:
+                        idx = np.random.randint(0, n)
+                    else:
+                        idx = np.random.choice(adj_list[idx])
+                page_rank[idx] += 1
+
+        page_rank /= (n * num_iter)
         return page_rank
